de2bd41393c695aa630165f3fb3b4d2395a67091
J.E. Alcalá <jaime.atemores@alumnos.udg.mx>
2023-11-15 21:48:03 -0600

    cambio de visibilidad; presentación en linea en io

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# sinca2023
- Simposio de sinca 2023
+#  Simposio de SINCA 2023
+
+[Discovering patterns in motion data using ~~supervised and~~ unsupervised models](https://jealcalat.github.io/sinca2023)
---
+++ libs/typed.js
@@ -0,0 +1,16 @@
+<script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.11"></script>
+
+<script>
+const options = {
+  strings: [
+    '<pre>nutriólogos</pre>', '<pre>psicólogos</pre>', '<pre>médicos</pre>', '<pre>físicos</pre>', '<pre>administradores</pre>', '<pre>contadores</pre>','<pre>médicos</pre>', '<pre>políticos</pre>', '<pre>ciudadanos</pre>'
+  ],
+  typeSpeed: 40,
+  backSpeed: 20,
+  cursorChar: '',
+  loop: true
+};
+
+const typed = new Typed('.typed', options);
+const typed2 = new Typed('.typed-eg', options);
+</script>
